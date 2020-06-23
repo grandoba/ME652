dec4d6231a2385b81ab381f1f398ddb7df14cda0
grandoba <49908185+grandoba@users.noreply.github.com>
2020-06-23 17:36:12 +0900

    finished problem 1

--- SLAM_skeleton.m
+++ SLAM_skeleton.m
@@ -3,7 +3,7 @@
 % Course instructor: Jinwhan Kim
 %==========================================================================
 
-function [] = SLAM_skeleton()
+function [] = SLAM_skeleton(noiseLevel)
 randn('seed',1);
 DTR = pi/180;   % degree to radian
 
@@ -25,11 +25,20 @@ Qe = zeros(14,14);
 
 % measurement noise
 Re = eye(12);
-Re(1,1) = (3*DTR)^2;
-Re(2,2) = 0.2^2;
-for i=1:5
-    Re(i*2+1,i*2+1) = (3*DTR)^2;
-    Re(i*2+2,i*2+2) = 0.2^2;
+if nargin == 0
+    Re(1,1) = (3*DTR)^2;
+    Re(2,2) = 0.2^2;
+    for i=1:5
+        Re(i*2+1,i*2+1) = (3*DTR)^2;
+        Re(i*2+2,i*2+2) = 0.2^2;
+    end
+else
+    Re(1,1) = (3*noiseLevel*DTR)^2;
+    Re(2,2) = (0.2*noiseLevel)^2;
+    for i=1:5
+        Re(i*2+1,i*2+1) = (3*noiseLevel*DTR)^2;
+        Re(i*2+2,i*2+2) = (0.2*noiseLevel)^2;
+    end
 end
 
 % initial estimate
@@ -45,15 +54,76 @@ for t=0:dt:10
 % -------------------------------------------------------------------------    
 %    Implement the following functions
 % -------------------------------------------------------------------------    
-%    z = generate_z(x);      % generate bearing and range measurements
-%    [xhat,Phat] = EKF_propagate(xhat,Phat,Qe,dt);  % state propagation
-%    [xhat,Phat] = EKF_update(xhat,z,Phat,Re,dt);   % measurement update
+   z = generate_z(x);      % generate bearing and range measurements
+   [xhat,Phat] = EKF_propagate(xhat,Phat,Qe,dt);  % state propagation
+   [xhat,Phat] = EKF_update(xhat,z,Phat,Re,dt);   % measurement update
 % -------------------------------------------------------------------------    
     clf
     show_data(t,x,xhat,Phat);
     drawnow;
 end
 
+function z = generate_z(x)
+%     disp('starting measurements ...')
+    z(1) = x(3); % vehicle bearing 
+    z(2) = x(4); % vehicle speed
+    for i = 1:5
+        % bearing 
+        z(2*i+1) = atan2( x(2*i+4)-x(2), x(2*i+3) -x(1))-x(3); 
+        % range
+        z(2*i+2) = norm( x(2*i+3:2*i+4) - x(1:2) );
+    end
+
+function  [xhat,Phat] = EKF_propagate(xhat,Phat, Qe, dt)
+%     disp('starting propagation ...')
+    Q = Qe*dt;
+    % A Jacobian 
+    A = jacob_A(xhat);
+    
+    Ak = eye(14)+A*dt;
+    Phat = Ak*Phat*Ak' + Q ;
+    xhat = propagate_x(xhat,dt);
+    
+function A = jacob_A(xhat)
+    A = zeros(14);
+    A(1,3) = - xhat(4)*sin(xhat(3));
+    A(2,3) = xhat(4)*cos(xhat(3));
+    A(1,4) = cos(xhat(3));
+    A(2,4) = sin(xhat(3));
+
+function [xhat,Phat] = EKF_update(xhat,z,Phat,Re,dt)  
+%     disp('starting update ...')
+    R = Re/dt;  
+    % H Jacobian 
+    H = jacob_H(xhat);  
+    % Kalman Gain
+    K = Phat*H'/(H*Phat*H'+R);                                                          
+    % update steps
+    zhat = generate_z(xhat); 
+    Phat = (eye(14) - K*H)*Phat;
+    xhat = xhat + K*(z - zhat)';
+
+function H = jacob_H(xhat)
+    H = zeros(12,14);
+    H(1,3) = 1;
+    H(2,4) = 1;
+    for i = 1:5
+        % xx = x_i - x_0
+        xx = xhat(2*i+3) - xhat(1);
+        % yy = y_i - y_0
+        yy = xhat(2*i+4) - xhat(2);
+        % rr = sqrt(xx^2 + yy^2)
+        rr = sqrt(xx^2 + yy^2);
+        
+        H(2*i+1,1) = yy/rr^2; 
+        H(2*i+1,2*i+3) = -yy/rr^2;
+        H(2*i+1,2) = -xx/rr^2;
+        H(2*i+1,2*i+4) = xx/rr^2;
+        H(2*i+2,1) = -xx/rr;
+        H(2*i+2,2*i+3) = xx/rr;
+        H(2*i+2,2) = -yy/rr;
+        H(2*i+2,2*i+4) = yy/rr;
+    end
 
 % state propagation
 %------------------------------------------------------------
